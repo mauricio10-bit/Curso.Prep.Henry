6163b8d48e45417c5501ddd880db30940fe1e99f
jesus mauricio pallares galindo <mauriciopallares10@gmail.com>
2022-11-19 00:22:22 -0500

    cambios realizados

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -28,8 +28,8 @@ const nuevoModulo = 21 % 5 === 1;
 function devolverString(str) {
   // "Return" la string provista: str
   // Tu código:
-  var str = '';
-  str;
+  var st = "Hola " + str;
+  return st;
 }
 
 
