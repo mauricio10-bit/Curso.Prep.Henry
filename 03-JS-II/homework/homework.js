2d3f10c582ff1dfa649c6854baaa88c3ec256cbd
jesus mauricio pallares galindo <mauriciopallares10@gmail.com>
2022-11-19 14:33:19 -0500

    Updated

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -172,7 +172,15 @@ function esPrimo(numero) {
   // Pista: un número primo solo es divisible por sí mismo y por 1
   // Pista 2: Puedes resolverlo usando un bucle `for`
   // Nota: Los números 0 y 1 NO son considerados números primos
-
+  if (numero < 2) {
+    return false
+  }
+  for (var i = 2; i < numero; i++) {
+    if (numero % i == 0) {
+      return false
+    }
+  }
+  return true
 }
 
 function esVerdadero(valor){
@@ -198,7 +206,7 @@ function tablaDelSeis(){
 function tieneTresDigitos(numero){
   //Leer un número entero y retornar true si tiene 3 dígitos. Caso contrario, retorna false.
   //Escribe tu código aquí
- if (numero.length == 3) {
+ if (numero.lengt == 3) {
   return true
  } else {
   return false
