8428d70a4e5f88aeb52cd455506ae8cb9c2de739
jesus mauricio pallares galindo <mauriciopallares10@gmail.com>
2022-11-19 14:07:50 -0500

    Updated

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -197,7 +197,11 @@ function tablaDelSeis(){
 function tieneTresDigitos(numero){
   //Leer un número entero y retornar true si tiene 3 dígitos. Caso contrario, retorna false.
   //Escribe tu código aquí
-  
+ if (numero.length == 3) {
+  return true
+ } else {
+  return false
+ }
 }
 
 function doWhile(numero) {
