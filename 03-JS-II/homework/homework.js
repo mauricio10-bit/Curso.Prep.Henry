41f462c3ce58e01663f479501f6e1d0f124e95bb
jesus mauricio pallares galindo <mauriciopallares10@gmail.com>
2022-11-19 14:23:12 -0500

    Updated

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -154,7 +154,8 @@ function operadoresLogicos(num1, num2, num3) {
       return "Hay negativos"
     } else {
       if (num3 > num1 && num3 > num2) {
-        return num3 + 1
+        num3 = num3 + 1
+        return num3
       }else{
         if (num1 > num2 && num1 > num3 && num1 > 0) {
           return "Número 1 es mayor y positivo"
